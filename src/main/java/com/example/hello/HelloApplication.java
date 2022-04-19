d8ec93a85611adc95392a3a2db952e0a57dcf10f
up1 <somkiat.p@gmail.com>
2022-04-20 02:59:06 +0700

    Add hello api

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,9 +2,17 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
